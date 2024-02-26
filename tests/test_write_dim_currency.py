a58f62ce503f697d075655a5b098d51646b0f661
jamesLeventis <jamesleventis@outlook.com>
2024-02-26 14:54:58 +0000

    add 2nd test for insertion

--- tests/test_write_dim_currency.py
+++ tests/test_write_dim_currency.py
@@ -9,8 +9,8 @@ from pg8000.native import Connection, literal, identifier, DatabaseError
 
 
 @pytest.mark.describe('write_dim_currency()')
-@pytest.mark.it('correct_data_is_written_to_DB')
-def test_correct_data_is_written_to_DB():
+@pytest.mark.it('test_number_of_times_connection_to_DB_is_made')
+def test_number_of_connections_to_DB():
     data = [
         {'currency_id': 1, 'currency_code': 'GBP', 'currency_name': 'British Pound'},
         {'currency_id': 2, 'currency_code': 'USD', 'currency_name': 'US Dollar'},
@@ -18,12 +18,47 @@ def test_correct_data_is_written_to_DB():
     ]
     con = Mock()
     mock_datetime = dt(2024, 2, 26, 10, 30, 0)
-    write_dim_currency(con, data, mock_datetime )
+    write_dim_currency(con, data, mock_datetime)
+
     assert con.run.call_count == len(data)
     
+
+# @pytest.mark.describe('write_dim_currency()')
+# @pytest.mark.it('correct_keys_are_added_to_DB')
+# def test_correct_keys_are_written_to_DB():
+#     data = [
+#         {'currency_id': 1, 'currency_code': 'GBP', 'currency_name': 'British Pound'},
+#         {'currency_id': 2, 'currency_code': 'USD', 'currency_name': 'US Dollar'},
+#         {'currency_id': 3, 'currency_code': 'EUR', 'currency_name': 'Euro'}
+#     ]
+#     con = Mock()
+#     mock_datetime = dt(2024, 2, 26, 10, 30, 0)
+#     result = write_dim_currency(con, data, mock_datetime)
+  
     
+#     assert 'currency_record_id' in con.run
+
+@pytest.mark.describe('write_dim_currency()')
+@pytest.mark.it('test_if_correct_data_is_being_inserted')
+def test_data_insertion():
+    data = [
+        {'currency_id': 1, 'currency_code': 'GBP', 'currency_name': 'British Pound'}]
+    con = Mock()
+    mock_datetime = dt(2024, 2, 26, 10, 30, 0)
+    write_dim_currency(con, data, mock_datetime)
+    expected = f"""
+        INSERT INTO dim_currency
+        VALUES
+        (1,1,
+        'GBP','British Pound',
+        '2024-02-26','10:30:00')
+        ON CONFLICT DO NOTHING;"""
+    assert con.run.call_args[0][0].strip() == expected.strip()
+
     
-    
+
+
+
     # for call_args, expected_query in zip(mock_connection.run.call_args_list, expected_queries):
     #     assert call_args[0][0] == expected_quer
     # expected = [f"""
