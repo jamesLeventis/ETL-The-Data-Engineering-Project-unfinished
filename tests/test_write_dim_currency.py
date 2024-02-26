60e8cac019ceb3717e26f6c2a92a03c94ad9369a
yilmazakdal <yilmazakdal@gmail.com>
2024-02-26 15:33:54 +0000

    added test_write_dim_currency

--- tests/test_write_dim_currency.py
+++ tests/test_write_dim_currency.py
@@ -22,22 +22,6 @@ def test_number_of_connections_to_DB():
 
     assert con.run.call_count == len(data)
     
-
-# @pytest.mark.describe('write_dim_currency()')
-# @pytest.mark.it('correct_keys_are_added_to_DB')
-# def test_correct_keys_are_written_to_DB():
-#     data = [
-#         {'currency_id': 1, 'currency_code': 'GBP', 'currency_name': 'British Pound'},
-#         {'currency_id': 2, 'currency_code': 'USD', 'currency_name': 'US Dollar'},
-#         {'currency_id': 3, 'currency_code': 'EUR', 'currency_name': 'Euro'}
-#     ]
-#     con = Mock()
-#     mock_datetime = dt(2024, 2, 26, 10, 30, 0)
-#     result = write_dim_currency(con, data, mock_datetime)
-  
-    
-#     assert 'currency_record_id' in con.run
-
 @pytest.mark.describe('write_dim_currency()')
 @pytest.mark.it('test_if_correct_data_is_being_inserted')
 def test_data_insertion():
@@ -55,21 +39,36 @@ def test_data_insertion():
         ON CONFLICT DO NOTHING;"""
     assert con.run.call_args[0][0].strip() == expected.strip()
 
-    
-
-
-
-    # for call_args, expected_query in zip(mock_connection.run.call_args_list, expected_queries):
-    #     assert call_args[0][0] == expected_quer
-    # expected = [f"""
-    #     INSERT INTO dim_currency
-    #     VALUES
-    #     (1,1,'GBP','British Pound','2024-02-26','10:30:0:0')
-    #     INSERT INTO dim_currency
-    #     VALUES
-    #     (2,2,'USD','US Dollar','2024-02-26','10:30:0:0')
-    #     INSERT INTO dim_currency
-    #     VALUES
-    #     (3,3,'EUR','Euro','2024-02-26','10:30:0:0')
-    #     ON CONFLICT DO NOTHING;
-    #     """ ]
+@pytest.mark.describe('write_dim_currency()')
+@pytest.mark.it('test_if_correct_data_is_being_inserted_for_multiple_data')
+def test_data_insertion_for_multiple_data():
+    data = [
+        {'currency_id': 1, 'currency_code': 'GBP', 'currency_name': 'British Pound'},
+        {'currency_id': 2, 'currency_code': 'USD', 'currency_name': 'US Dollar'},
+        {'currency_id': 3, 'currency_code': 'EUR', 'currency_name': 'Euro'}
+    ]
+    con = Mock()
+    mock_datetime = dt(2024, 2, 26, 10, 30, 0)
+    write_dim_currency(con, data, mock_datetime)
+    expected = [f"""
+        INSERT INTO dim_currency
+        VALUES
+        (1,1,
+        'GBP','British Pound',
+        '2024-02-26','10:30:00')
+        ON CONFLICT DO NOTHING;""",
+        """INSERT INTO dim_currency
+        VALUES
+        (2,2,
+        'USD','US Dollar',
+        '2024-02-26','10:30:00')
+        ON CONFLICT DO NOTHING;""",
+        """INSERT INTO dim_currency
+        VALUES
+        (3,3,
+        'EUR','Euro',
+        '2024-02-26','10:30:00')
+        ON CONFLICT DO NOTHING;
+        """]
+    for i,data_point in enumerate(data):
+        assert con.run.call_args_list[i][0][0].strip() == expected[i].strip()
